1b6f94215388443912229a60a85100f335c2fd7f
acieza <asanjuancieza@gmail.com>
2021-02-09 20:08:27 +0100

    3333

--- src/app/admin/usuarios/usuarios.component.ts
+++ src/app/admin/usuarios/usuarios.component.ts
@@ -31,6 +31,9 @@ export class UsuariosComponent implements OnInit {
     this.authService.getAllUsuarios()
     .subscribe(usuarios =>{
       this.usuarios = usuarios;
+      console.log(this.usuarios);
+      this.filteredItems = this.usuarios;
+      this.init();
     })
   }
 
@@ -70,6 +73,84 @@ export class UsuariosComponent implements OnInit {
        
       }
 
+      /****************************************/
 
+      filteredItems: Usuario[];
+  pages: number = 4;
+  pageSize: number = 5;
+  pageNumber: number = 0;
+  currentIndex: number = 1;
+  items: Usuario[];
+  pagesIndex: Array<number>;
+  pageStart: number = 1;
+  inputName: string = '';
+
+ 
+  init() {
+    this.currentIndex = 1;
+    this.pageStart = 1;
+    this.pages = 4;
+
+    this.pageNumber = parseInt("" + (this.filteredItems.length / this.pageSize));
+    if (this.filteredItems.length % this.pageSize != 0) {
+      this.pageNumber++;
+    }
+
+    if (this.pageNumber < this.pages) {
+      this.pages = this.pageNumber;
+    }
+
+    this.refreshItems();
+    console.log("this.pageNumber :  " + this.pageNumber);
+  }
+
+  FilterByName() {
+    this.filteredItems = [];
+    if (this.inputName != "") {
+      this.usuarios.forEach(element => {
+        if (element.nombre.toUpperCase().indexOf(this.inputName.toUpperCase()) >= 0) {
+          this.filteredItems.push(element);
+        }
+      });
+    } else {
+      this.filteredItems = this.usuarios;
+    }
+    console.log(this.filteredItems);
+    this.init();
+  }
+  fillArray(): any {
+    var obj = new Array();
+    for (var index = this.pageStart; index < this.pageStart + this.pages; index++) {
+      obj.push(index);
+    }
+    return obj;
+  }
+  refreshItems() {
+    this.items = this.filteredItems.slice((this.currentIndex - 1) * this.pageSize, (this.currentIndex) * this.pageSize);
+    this.pagesIndex = this.fillArray();
+  }
+  prevPage() {
+    if (this.currentIndex > 1) {
+      this.currentIndex--;
+    }
+    if (this.currentIndex < this.pageStart) {
+      this.pageStart = this.currentIndex;
+    }
+    this.refreshItems();
+  }
+  nextPage() {
+    if (this.currentIndex < this.pageNumber) {
+      this.currentIndex++;
+    }
+    if (this.currentIndex >= (this.pageStart + this.pages)) {
+      this.pageStart = this.currentIndex - this.pages + 1;
+    }
+
+    this.refreshItems();
+  }
+  setPage(index: number) {
+    this.currentIndex = index;
+    this.refreshItems();
+  }
 }
 
